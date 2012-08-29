f3af697a1b9071d7165ae5e66173b5f67f8754d1
Pierre Lepage <egroupcorp@gmail.com>
2012-08-29 09:56:11 -0700

    Revert "Revert "Revert "initial commit"""
    This reverts commit a8b5692974a32b83e330bae3ca56295d6e78b394.

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-FirstApp::Application.config.secret_token = '80baecda37c461acf873d55a73277c01f589accc784753213be23d74eeac50be4e6181c969501a10c2ce79ba727e47b8d83601f29976684104f0c3c6d3b8006f'
